1795b0624796857de735406dd413d49f90d4472d
Samuel Hetteš <samuel.hettes@gmail.com>
2024-04-02 11:58:45 +0200

    ambulance waiting list

--- internal/ambulance_wl/impl_ambulances.go
+++ internal/ambulance_wl/impl_ambulances.go
@@ -84,5 +84,52 @@ func (this *implAmbulancesAPI) CreateAmbulance(ctx *gin.Context) {
 
 // DeleteAmbulance - Deletes specific ambulance
 func (this *implAmbulancesAPI) DeleteAmbulance(ctx *gin.Context) {
-   ctx.AbortWithStatus(http.StatusNotImplemented)
+	value, exists := ctx.Get("db_service")
+	if !exists {
+		ctx.JSON(
+			http.StatusInternalServerError,
+			gin.H{
+					"status":  "Internal Server Error",
+					"message": "db_service not found",
+					"error":   "db_service not found",
+			})
+		return
+	}
+
+	db, ok := value.(db_service.DbService[Ambulance])
+	if !ok {
+		ctx.JSON(
+			http.StatusInternalServerError,
+			gin.H{
+					"status":  "Internal Server Error",
+					"message": "db_service context is not of type db_service.DbService",
+					"error":   "cannot cast db_service context to db_service.DbService",
+			})
+		return
+	}
+
+	ambulanceId := ctx.Param("ambulanceId")
+	err := db.DeleteDocument(ctx, ambulanceId)
+
+	switch err {
+	case nil:
+		ctx.AbortWithStatus(http.StatusNoContent)
+	case db_service.ErrNotFound:
+		ctx.JSON(
+			http.StatusNotFound,
+			gin.H{
+					"status":  "Not Found",
+					"message": "Ambulance not found",
+					"error":   err.Error(),
+			},
+		)
+	default:
+		ctx.JSON(
+			http.StatusBadGateway,
+			gin.H{
+					"status":  "Bad Gateway",
+					"message": "Failed to delete ambulance from database",
+					"error":   err.Error(),
+			})
+	}
 }
